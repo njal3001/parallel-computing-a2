fa75ed11c9abced6d9d9f4b1d38296b9cc1a5c49
Nguyen Son Linh <linhns59@gmail.com>
2022-10-10 17:16:10 +0800

    Super slow version

--- src/kernel.cu
+++ src/kernel.cu
@@ -34,12 +34,41 @@
 */
 
 #include <vector>
+#include <sstream>
+#include <iomanip>
 
 #include "defs.h"
 
-__global__ void matchFile(const uint8_t* file_data, size_t file_len, const char* signature, size_t len)
+
+// Kernel to test if 2 strings are equal
+__device__ bool equal(const char* a, const char* b, size_t len) {
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    bool result = true;
+    if (i < len) {
+        if (a[i] != b[i]) result = false;
+    }
+    return result;
+}
+
+__device__ bool d_result;
+
+__global__ void matchFile(const char* file_data, size_t file_len, const char* signature, size_t len)
 {
-	// TODO: your code!
+	for (int i = 0; i < file_len - len + 1; i++) {
+        bool tmp = equal(file_data + i, signature, len);
+        if (!tmp) d_result = false;
+    }
+}
+
+// Convert array of hexadecimal numbers to char*
+char* to_hex(uint8_t* data, size_t len) {
+  	std::ostringstream oss;
+  	oss << std::hex << std::setfill('0');
+  	for (size_t i = 0; i < len; i++) {
+    	oss << std::setw(2) << (unsigned int)data[i];
+  	}
+	auto s = oss.str();
+  	return &s[0];
 }
 
 void runScanner(std::vector<Signature>& signatures, std::vector<InputFile>& inputs)
@@ -62,13 +91,13 @@ void runScanner(std::vector<Signature>& signatures, std::vector<InputFile>& inpu
 	std::vector<cudaStream_t> streams {};
 	streams.resize(inputs.size());
 
-	std::vector<uint8_t*> file_bufs {};
+	std::vector<char*> file_bufs {};
 	for(size_t i = 0; i < inputs.size(); i++)
 	{
 		cudaStreamCreate(&streams[i]);
 
 		// allocate memory on the device for the file
-		uint8_t* ptr = 0;
+		char* ptr = 0;
 		check_cuda_error(cudaMalloc(&ptr, inputs[i].size));
 		file_bufs.push_back(ptr);
 	}
@@ -85,16 +114,22 @@ void runScanner(std::vector<Signature>& signatures, std::vector<InputFile>& inpu
 
 	for(size_t file_idx = 0; file_idx < inputs.size(); file_idx++)
 	{
+		auto input = to_hex(inputs[file_idx].data, inputs[file_idx].size);
 		// asynchronously copy the file contents from host memory
 		// (the `inputs`) to device memory (file_bufs, which we allocated above)
-		cudaMemcpyAsync(file_bufs[file_idx], inputs[file_idx].data, inputs[file_idx].size,
+		cudaMemcpyAsync(file_bufs[file_idx], input, inputs[file_idx].size,
 			cudaMemcpyHostToDevice, streams[file_idx]);    // pass in the stream here to do this async
 
 		for(size_t sig_idx = 0; sig_idx < signatures.size(); sig_idx++)
 		{
 			// launch the kernel!
 			// your job: figure out the optimal dimensions
+			int threadsPerBlock = 256;
+			auto sz = signatures[sig_idx].size;
+			int blocksPerGrid = (sz + threadsPerBlock - 1) / threadsPerBlock;
 
+			bool result = true;
+    		cudaMemcpyToSymbol(d_result, &result, sizeof(bool));
 			/*
 				This launch happens asynchronously. This means that the CUDA driver returns control
 				to our code immediately, without waiting for the kernel to finish. We can then
@@ -108,13 +143,16 @@ void runScanner(std::vector<Signature>& signatures, std::vector<InputFile>& inpu
 				128 kernels can run concurrently --- subject to resource constraints. This means
 				you should *definitely* be doing more work per kernel than in our example!
 			*/
-			matchFile<<<1, 1, /* shared memory per block: */ 0, streams[file_idx]>>>(
+			matchFile<<<blocksPerGrid, threadsPerBlock, /* shared memory per block: */ 0, streams[file_idx]>>>(
 				file_bufs[file_idx], inputs[file_idx].size,
 				sig_bufs[sig_idx], signatures[sig_idx].size);
-
+			
+			cudaMemcpyFromSymbol(&result, d_result, sizeof(bool));
 
 			// example output printing. don't forget to change this!
-			printf("%s: %s\n", inputs[file_idx].name.c_str(), signatures[sig_idx].name.c_str());
+			if (result) {
+				printf("%s: %s\n", inputs[file_idx].name.c_str(), signatures[sig_idx].name.c_str());
+			}
 		}
 	}
 
